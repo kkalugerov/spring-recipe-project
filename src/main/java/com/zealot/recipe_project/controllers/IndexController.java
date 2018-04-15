b1f9b14208e7ec29a3ecb19eeaf2137eded44396
kkalugerov <k.kalugerov@yatrusanalytics.com>
2018-04-15 12:06:12 +0300

    initial commit

---
+++ src/main/java/com/zealot/recipe_project/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.zealot.recipe_project.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage() {
+        return "index";
+    }
+}
